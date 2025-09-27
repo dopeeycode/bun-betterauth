de0d5ee25a67067094aae3b4ddf034072b650429
dopeey <italodopeey@gmail.com>
2025-09-27 05:09:04 -0400

    feat: integrate better-auth and drizzle-orm for authentication and database management
    - Added better-auth for authentication with drizzle-orm adapter.
    - Configured drizzle-orm with PostgreSQL and defined database schema.
    - Created migration files for users, sessions, accounts, and verifications tables.
    - Set up environment variables for database connection and authentication.
    - Updated package.json with new dependencies and scripts for database generation and migration.
    - Added docker-compose configuration for PostgreSQL service.

---
+++ src/database/client.ts
@@ -0,0 +1,10 @@
+import { env } from '@/env'
+import { drizzle } from 'drizzle-orm/node-postgres'
+
+import { schema } from './schema'
+
+
+export const db = drizzle(env.DATABASE_URL, {
+  schema,
+  casing: 'snake_case'
+})
